3a945f424dba71ac9f289dafcb24d74775e172f2
jgreen44 <ShallowDoF18!>
2022-12-16 04:18:47 -0600

    WIP

--- src/libs/sdk-http/requests.ts
+++ src/libs/sdk-http/requests.ts
@@ -1,19 +1,18 @@
 import axios from 'axios';
 
 export const sendOTPEmail = async () => {
+  const otp = Math.floor(Math.random() * 1000000);
   try {
     const response = await axios.post(
       'https://4edl8sk54b.execute-api.us-east-2.amazonaws.com/dev',
       {
-        data: {
-          opt: '123',
-        },
+        otp,
         headers: {
           'Content-Type': 'application/json',
         },
       }
     );
-    alert(response);
+    alert(response.data);
   } catch (e) {
     console.log(e);
   }
